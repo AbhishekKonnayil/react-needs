6574123147b1b7d0f462fc8897ee6527ff3417ed
Abhishek <abhishek.konnayil@gmail.com>
2025-06-05 12:00:56 +0530

    Refactor App component to implement counter functionality with useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useEffect, useState } from 'react';
-import FunctionComponent from './FunctionComponent';
-import ClassComponentContext from './ClassContextComponent';
-import React from 'react';
-import { ThemeProvider } from './ThemeContext';
+import { useReducer, useState } from 'react';
 
 
-export const ThemeContext = React.createContext()
+
+
+function reducer(state, Action) {
+  switch (Action.type) {
+
+    case 'increment': {
+      const hasError = state.count >= 5
+      const newCount = state.count + 1
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? 'Maximum count reached' : null
+      }
+    }
+
+    case 'decrement': {
+      const hasError = state.count <= 0
+      const newCount = state.count - 1
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? 'Minimum count reached' : null
+      }
+    }
+
+    default:
+      return state
+  }
+}
+
 
 const App = () => {
-
-
-
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
 
   return (
-    <>
-      <ThemeProvider>
-        <FunctionComponent />
-      </ThemeProvider>
-
-    </>
+    <div className="App">
+      <h1>Counter</h1>
+      <p>Count: {state.count}</p>
+      {state.error && <p className='text-red-400 text-xl font-bold'>{state.error}</p>}
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+    </div>
   )
 }
 export default App;
